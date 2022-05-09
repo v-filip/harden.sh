6437fe7b7b3bb104b5dfbe5966138e798f85c1e7
v-filip <v-filip@null.com>
2022-05-09 09:50:23 +0200

    First part of the menu added
    Code still to be written for the third function, leaving it for the end because its going to be the longest function (all of them still need to be tested)

--- harden.sh
+++ harden.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 #Author: v-filip
 
+function sshd_status {
+	systemctl status ssh || service ssh status
+}
+
+function sshd_restart {
+	systemctl restart ssh || service ssh restart
+}
+
+function config_ssh {
+	#code
+}
+
 while true
 do
 	echo "----------------------------"
-	echo "SSH OPTIONS:"
+	echo "SSH OPTIONS"
 	echo "1a) SSHd status"
 	echo "2a) SSHd restart"
 	echo "3a) Configure SSH"
 	echo "----------------------------"
+	echo "FIREWALL OPTIONS"
 	echo "1b) UFW status"
 	echo "2b) UFW enable"
 	echo "3b) UFW disable"
@@ -16,22 +29,28 @@ do
 	echo "5b) Allow/Deny inbound port"
 	echo "x) exit"
 	echo "------_---------------------"
-	read -p "Please make your selection: " ANSWER
+	read -p "Please make your selection: [ex. 1a] " ANSWER
 	echo
 	case $ANSWER in 
 		1a)
-			#sshd status
-			#function
+			sshd_status
+			echo "-------------------"
+			echo "SSH status displyed"
+			echo "-------------------"
 			;;
 		
 		2a)
-			#sshd restart
-			#function
+			sshd_restart
+			echo "---------------------"
+			echo "SSH daemon restarted!"
+			echo "---------------------"
 			;;
 
 		3a)
-			#configure ssh
-			#function
+			config_ssh
+			echo "---------------------"
+			echo "Done! SSH configured!"
+			echo "---------------------"
 			;;
 #		4a)
 #			#extra
@@ -68,12 +87,15 @@ do
 			echo "--------------------------------------"
 			echo "Thank you for using v-filip/harden.sh!"
 			echo "--------------------------------------"
+			echo
 			sleep 1
 			break
 			;;
 
 		*)
-			#invlaid input#
+			echo "----------------------------------------------------------------------"
+			echo "Invalid input, please use one of the provided selections (Example. 1)!"
+			echo "----------------------------------------------------------------------"
 			;;
 	esac
 done
